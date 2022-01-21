0fe738daa5ef2c55409b218b5ad0dd9b388bdbee
andywokzyrg <andywokzyrg>
2022-01-21 17:25:24 +0300

    + main-layout

--- ng-portfoolio/src/app/app-routing.module.ts
+++ ng-portfoolio/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+import {AboutPageComponent} from "./about-page/about-page.component";
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {MusicPageComponent} from "./music-page/music-page.component";
+import {ProjectsPageComponent} from "./projects-page/projects-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '', component: MainLayoutComponent, children: [
+        {path: '', redirectTo: '/', pathMatch: 'full'},
+        {path: '', component: HomePageComponent},
+        {path: 'about', component: AboutPageComponent},
+        {path: 'contact', component: ContactPageComponent},
+        {path: 'music', component: MusicPageComponent},
+        {path: 'projects', component: ProjectsPageComponent}
+    ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-portfoolio/src/app/app.module.ts
+++ ng-portfoolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MusicPageComponent } from './music-page/music-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    MusicPageComponent,
+    ProjectsPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
