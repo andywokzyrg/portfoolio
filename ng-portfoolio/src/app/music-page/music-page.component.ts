9d647bf69cf715c05b6fb4e9ab84e0df6fded8cc
andywokzyrg <andywokzyrg>
2022-01-23 17:21:35 +0300

    + music-page component

--- ng-portfoolio/src/app/music-page/music-page.component.ts
+++ ng-portfoolio/src/app/music-page/music-page.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-music-page',
-  templateUrl: './music-page.component.html',
-  styleUrls: ['./music-page.component.scss']
+    selector: 'app-music-page',
+    templateUrl: './music-page.component.html',
+    styleUrls: ['./music-page.component.scss']
 })
 export class MusicPageComponent implements OnInit {
 
-  constructor() { }
+    old = true
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
