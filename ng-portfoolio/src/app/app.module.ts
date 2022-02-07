eea7a4815c1ca418cc247e37cf59831c7db9e847
andywokzyrg <andywokzyrg>
2022-02-07 13:42:06 +0300

    refactor

--- ng-portfoolio/src/app/app.module.ts
+++ ng-portfoolio/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {AboutPageComponent} from './about-page/about-page.component';
@@ -9,6 +9,7 @@ import {ContactPageComponent} from './contact-page/contact-page.component';
 import {MusicPageComponent} from './music-page/music-page.component';
 import {ProjectsPageComponent} from './projects-page/projects-page.component';
 import {HomePageComponent} from './home-page/home-page.component';
+
 import {ContactLinkModule} from "./shared/components/ui/contact-link/contact-link.module";
 import {ButtonModule} from "./shared/components/ui/button/button.module";
 import {IconModule} from "./shared/components/ui/icon/icon.module";
@@ -23,7 +24,6 @@ import {IconModule} from "./shared/components/ui/icon/icon.module";
         MusicPageComponent,
         ProjectsPageComponent,
         HomePageComponent,
-
     ],
     imports: [
         BrowserModule,
